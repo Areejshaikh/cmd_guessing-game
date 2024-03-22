800dde1e2e142900affbabc7607f4edcdba5d771
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-03-23 01:11:24 +0500

    creatingNumberGuessingGame

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+// 1) computer will generate a ramdom number : done
+// 2) User input for gusing number : done
+// 3) Compare user input with conputer generated number and show result : done
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessNumber",
+    type: "number",
+    message: "Please guess a number between 1 to 6:",
+  },
+]);
+if (answer.userGuessNumber === randomNumber) {
+  console.log("Congratulations! You guessd right number");
+} else {
+  console.log("You guessd wrong number");
+}
